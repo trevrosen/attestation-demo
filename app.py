23da62d5587c95d42898cf8106e3ab65e611f578
Trevor Rosen <trevrosen@github.com>
2025-06-26 10:40:57 -0500

    Make a change to hello
    Signed-off-by: Trevor Rosen <trevrosen@github.com>

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Attested World!'
+    return 'Hello, Attested World - we love K8s!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5005)
